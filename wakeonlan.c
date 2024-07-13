5fc6e26c4c99eed25a582c8e87a400c0b3fae830
Mateus Salvi <mateusalvi@gmail.com>
2024-07-13 16:29:51 -0300

    Main loop

--- wakeonlan.c
+++ wakeonlan.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "monitoring.h"
 #include "discovery.h"
 #include "management.h"
 #include "interface.h"
 
-void main()
+int main(int argc, char *argv[])
 {
-    
+    char *argument = argv[1];
+
+    if(*argument == 's' || *argument == 'S') 
+    {
+        while(1)
+        {
+
+        }
+    }
+    else
+    {
+        printf("Invalid or no argument passed, running as Client mode. (Launch with s or S arguments to force Server mode)\n");
+        while(1)
+        {
+
+        }
+    }
+
+    return 0;
 }
